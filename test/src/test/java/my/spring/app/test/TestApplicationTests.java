fea8af8edee45b76ebafec3b30e6f53e7a3778ff
MSBear007 <igmedv2020@gmail.com>
2022-10-10 11:21:13 +0300

    Initializr

---
+++ test/src/test/java/my/spring/app/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package my.spring.app.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
