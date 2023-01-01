32b062087c2c6ffb37f7a4b4aa3d5b777f39b6bc
MSBear007 <igmedv2020@gmail.com>
2023-01-01 14:25:29 +0300

    actually string in birthDate are better
    This reverts commit 11b4fb996261dda3c4a5d8de97277fc21b6c1480.

--- test/src/test/java/my/spring/app/test/ActorControllerTests.java
+++ test/src/test/java/my/spring/app/test/ActorControllerTests.java
@@ -1,19 +1,18 @@
 package my.spring.app.test;
 
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.mock.web.MockMultipartHttpServletRequest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
@@ -27,6 +26,9 @@ import my.spring.app.test.restapi.model.Actor;
 import my.spring.app.test.restapi.model.Country;
 import my.spring.app.test.restapi.repositories.ActorRepository;
 import my.spring.app.test.restapi.service.ActorService;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.*;
 
 @WebMvcTest(ActorController.class)
 public class ActorControllerTests {
@@ -82,5 +84,13 @@ public class ActorControllerTests {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name", is("George")));
         
+    }
+
+    static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
